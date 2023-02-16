1a617a873f2d3351ed46d0cb598a4d91db831355
Michał Maciejewski <maciejewskimichal7@gmail.com>
2023-02-16 16:10:18 +0000

    add project files

---
+++ src/Form.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Props = {}
+
+export const Form = (props: Props) => {
+  return (
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Name" {...register("Name", {required: true, min: 5, maxLength: 80})} />
+        <input type="text" placeholder="Email" {...register("Email", {required: true, min: 5, pattern: /^\S+@\S+$/i})} />
+        <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, maxLength: 12})} />
+
+        <input type="submit" />
+        </form>
+    );
+  )
+}
---
+++ src/components/Switcher.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export const Switcher = (props: Props) => {
+  return (
+    <div>Switcher</div>
+  )
+}
